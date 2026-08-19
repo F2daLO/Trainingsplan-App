88d179ed770af3a17614ac35da733c2b80e5273d
F2daLO <98152444+F2daLO@users.noreply.github.com>
2026-08-19 14:25:55 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="training-v6-5-2-equipment-ux";
+const CACHE="training-v6-5-3-equipment-stable";
 const ASSETS=["./","./index.html","./manifest.json","./icon.svg"];
 
 self.addEventListener("install",event=>{
