6ff4416d9f799a89ab0d7e9cf01d05163e56185e
F2daLO <98152444+F2daLO@users.noreply.github.com>
2026-09-16 10:10:54 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="training-v6-11-analytics-gpt-ios27";
+const CACHE="training-v6-12-analytics-ui";
 const ASSETS=["./","./index.html","./manifest.json","./icon.svg"];
 
 self.addEventListener("install",event=>{
