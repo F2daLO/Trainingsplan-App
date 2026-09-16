47a5f3b02970c6d5058b06685272a90a24bec689
F2daLO <98152444+F2daLO@users.noreply.github.com>
2026-09-16 16:40:16 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="training-v6-21-plan-package";
+const CACHE="training-v6-21-1-gpt-header-actions";
 const ASSETS=["./","./index.html","./manifest.json","./icon.svg"];
 
 self.addEventListener("install",event=>{
