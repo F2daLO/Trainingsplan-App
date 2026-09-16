8ef0acd2251fa6812b99234da0d9d286f7de2681
F2daLO <98152444+F2daLO@users.noreply.github.com>
2026-09-16 13:24:04 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="training-v6-17-native-dropdown-restore";
+const CACHE="training-v6-18-1-plan-today-reset";
 const ASSETS=["./","./index.html","./manifest.json","./icon.svg"];
 
 self.addEventListener("install",event=>{
