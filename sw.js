903551368c7d9b21a0a7e0b3af1d10eaff6887db
F2daLO <98152444+F2daLO@users.noreply.github.com>
2026-09-16 11:05:56 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="training-v6-13-2-liquidglass-filter";
+const CACHE="training-v6-13-3-clear-footer";
 const ASSETS=["./","./index.html","./manifest.json","./icon.svg"];
 
 self.addEventListener("install",event=>{
