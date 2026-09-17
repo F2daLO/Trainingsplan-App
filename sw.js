9df49c2400e90a57a7124a08c6112104f661a9ff
F2daLO <98152444+F2daLO@users.noreply.github.com>
2026-09-17 07:21:25 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="training-v6-24-auto-coach";
+const CACHE="training-v6-25-coach-screen";
 const ASSETS=["./","./index.html","./manifest.json","./icon.svg"];
 
 self.addEventListener("install",event=>{
