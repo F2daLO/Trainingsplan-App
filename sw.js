af0a4893e4899633c25ff2148571ffcc331aa3ff
F2daLO <98152444+F2daLO@users.noreply.github.com>
2026-09-17 14:13:28 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "mein-training-v6.31";
+const CACHE_NAME = "mein-training-v6.32";
 const APP_SHELL = ["./", "./index.html", "./manifest.json", "./icon.svg"];
 
 self.addEventListener("install", event => {
